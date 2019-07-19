d562d9bbbc899e7abd0272ba085a83e0febc7a85
Valentin Dufois <valentin@dufois.fr>
2019-07-19 16:57:21 +0200

    WIP

--- app/NewsSubject.php
+++ app/NewsSubject.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class NewsSubject extends Model
 {
-    protected $table = "news_subject";
+    protected $table = "news_subjects";
 
     public function records() {
         return $this->hasMany('App\NewsRecord');
