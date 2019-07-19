b3b64fdaf4aebd061c3309b9033d5c75349c09de
Valentin Dufois <valentin@dufois.fr>
2019-07-19 18:21:32 +0200

    WIP

--- app/NewsRecord.php
+++ app/NewsRecord.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class NewsRecord extends Model
 {
     protected $table = "news_records";
-    protected $fillable = "cp_id";
+    protected $fillable = ["cp_id"];
 }
