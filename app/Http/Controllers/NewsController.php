c1889b8ba50171ffdb48322bd05cf077de5a1f18
Valentin Dufois <valentin@dufois.fr>
2019-07-19 17:50:30 +0200

    WIP

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -41,10 +41,10 @@ class NewsController extends Controller
                 $articleXML = simplexml_load_string($cpStorage->get($article));
 
                 $articleInfos = [
-                    'id' => $articleXML->xpath('//doc-id/@id-string')[0][0][0],
-                    'date' => $articleXML->xpath('//story.date/@norm'),
-                    'headline' => $articleXML->xpath('//hl1'),
-                    'media' => $articleXML->xpath('//media-reference/@source'),
+                    'id' => $articleXML->xpath('//doc-id/@id-string')[0][0][0][0],
+                    'date' => $articleXML->xpath('//story.date/@norm')[0][0][0][0],
+                    'headline' => $articleXML->xpath('//hl1')[0][0],
+                    'media' => $articleXML->xpath('//media-reference/@sourceqew'),
                 ];
 
 //                $articleInfos = $parser->mask([
