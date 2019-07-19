0d43be6440d7af9d288da25cccd586970ca68070
Valentin Dufois <valentin@dufois.fr>
2019-07-19 19:02:34 +0200

    WIP

--- app/NewsRecord.php
+++ app/NewsRecord.php
@@ -8,4 +8,8 @@ class NewsRecord extends Model
 {
     protected $table = "news_records";
     protected $fillable = ["cp_id", "subject", "locale", "headline", "date", "media"];
+
+    public function subject() {
+        return $this->hasOne('App\NewsSubject');
+    }
 }
