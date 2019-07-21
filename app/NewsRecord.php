532a14638a9dbe72eb1083754048461a62013239
Valentin Dufois <valentin@dufois.fr>
2019-07-21 17:08:02 +0200

    Adding record media path to NewsRecords Model

--- app/NewsRecord.php
+++ app/NewsRecord.php
@@ -3,13 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class NewsRecord extends Model
 {
     protected $table = "news_records";
     protected $fillable = ["cp_id", "subject", "locale", "headline", "date", "media"];
+    protected $appends = array('path');
 
     public function subject() {
         return $this->hasOne('App\NewsSubject');
+    }
+
+    public function getPathAttribute() {
+        return Storage::disk('public')->url('news/medias/'.$this->media);
     }
 }
