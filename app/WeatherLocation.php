bf76d863fdda7234357dee6852a0bc9aec4b4b59
Valentin Dufois <valentin@dufois.fr>
2020-03-19 12:15:24 -0400

    bugfix

--- app/WeatherLocation.php
+++ app/WeatherLocation.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class WeatherLocation extends Model
 {
-	protected $fillable = array('country', 'province', 'city', 'selection', 'revert_date');
+	protected $fillable = array('country', 'province', 'city', 'selection', 'revert_date', 'endpoint');
 
 	public function backgrounds() {
     	return $this->hasMany('App\WeatherBackground', 'location');
