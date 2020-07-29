5cd40e4544cf1f6c2df619a9f80edc654bfceb87
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2020-07-29 14:21:01 -0400

    test

--- app/Http/Middleware/CORSMiddleware.php
+++ app/Http/Middleware/CORSMiddleware.php
@@ -26,7 +26,7 @@ class CORSMiddleware
 
 		if ($request->getMethod() == "OPTIONS") {
 			// The client-side application can set only headers allowed in Access-Control-Allow-Headers
-			return Response::make('OK', 200, $headers);
+			return Response::make('OK', 200);
 		}
 
 		$response = $next($request);
