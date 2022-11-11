0657705438f1d2cb228eebed48598c14205f6a89
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2022-11-11 10:31:53 -0500

    temp: Disable meteo media queries temporarily

--- app/Http/Middleware/WeatherControlMiddleware.php
+++ app/Http/Middleware/WeatherControlMiddleware.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Response;
+use Illuminate\Http\Response;
 
 class WeatherControlMiddleware {
     public function handle(Request $request, Closure $next) {
